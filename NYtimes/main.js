61a7f4e0d1218b6fddae7b0704fe947ee340f1a8
geunseonkim <guensoen@gmail.com>
2024-02-20 12:02:09 -0800

    second edit

--- NYtimes/main.js
+++ NYtimes/main.js
@@ -1,7 +1,7 @@
 const API_KEY = `68c442d8c9a541c0b4bb04244d50aea2`;
 let news = [];
 const getLatestNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://https://javas-project-jane.netlify.app/nytimes//top-headlines?country=us&apiKey=${API_KEY}`);
     const response = await fetch(url); // 비동기.
     const data = await response.json();
     news = data.articles;
