b8d72a9d2fe1f57daab61d99d6ae78e24b8bd5f3
geunseonkim <guensoen@gmail.com>
2024-02-20 12:07:45 -0800

    fourth

--- NYtimes/main.js
+++ NYtimes/main.js
@@ -1,7 +1,7 @@
 const API_KEY = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
 let news = [];
 const getLatestNews = async () => {
-    const url = new URL(`https://https://javas-project-jane.netlify.app/nytimes//top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://https://javas-project-jane.netlify.app/nytimes//top-headlines?`);
     const response = await fetch(url); // 비동기.
     const data = await response.json();
     news = data.articles;
